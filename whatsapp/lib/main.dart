c73a82ea995ad5e4d6d56f09d1b4e812da5d06aa
RafaelHernandezVega <themetallicstar@gmail.com>
2019-11-29 14:50:46 -0600

    final

--- whatsapp/lib/main.dart
+++ whatsapp/lib/main.dart
@@ -1,232 +1,202 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
-void main() => runApp(WhatsApp());
+void main() => runApp(Chat());
 
-const String name = "Contacto";
 final bd = Firestore.instance;
-int id = 0;
 
-class WhatsApp extends StatelessWidget {
+class Chat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MediaQuery(
-        data: new MediaQueryData(), child: new MaterialApp(home: new Estado(),debugShowCheckedModeBanner: false,));
-
+    return Estado();
   }
 }
 
-
-
-class Estado extends StatefulWidget{
+class Estado extends StatefulWidget {
   @override
-  State createState() => new ChatScreenState();
+  State createState() => new ChatState();
 }
 
-
-
-class ChatScreenState extends State<Estado> {
-
-  final List<ChatMessage> messages = <ChatMessage>[];
+class ChatState extends State<Estado> {
   final textController = TextEditingController();
 
-  @override
-  Widget build(BuildContext context) {
-
-    var a = obtener();
-    do {
-      var a = obtener();
-      if (a != null){
-        messages.add(ChatMessage(text: a));
-      }
-    } while (a != null);
-
-    return new Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(7, 94, 84, 50),
-
-        leading: IconButton(
-            icon: Icon(Icons.account_circle, color: Colors.blue, size: 40,)),
-
-        title: Column(
-          verticalDirection: VerticalDirection.down,
+  Widget buildItem(int index, DocumentSnapshot document) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Container(
+        width: 200.0,
+        padding: EdgeInsets.fromLTRB(200.0, 0, 0.0, 0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
             Container(
-              alignment: Alignment(-1, 0),
-              child: Text('Contacto', style: TextStyle(color: Colors.white, fontSize: 20,),),
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(10, 10, 10, .1),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.only(
+                        right: 12, left: 12, top: 7, bottom: 5.0),
+                    child: Text(
+                      document.data['Mensaje'],
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
             Container(
-              alignment: Alignment(-1, 0),
-              child:  Text('últ. vez hoy a la 1:42 p.m.', style: TextStyle(color: Colors.white, fontSize: 15),),
-            ),
+                margin: EdgeInsets.only(left: 5),
+                child: Icon(
+                  Icons.account_circle,
+                  color: Colors.blue,
+                  size: 40,
+                )),
           ],
         ),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.videocam, color: Colors.white,),),
-          IconButton(icon: Icon(Icons.call, color: Colors.white,),),
-          IconButton(icon: Icon(Icons.more_vert, color: Colors.white,),)
-        ],
-      ),
-      body: Column(
-        children: <Widget>[
-          Flexible(
-            child: ListView.builder(
-              padding: EdgeInsets.all(20.0),
-              reverse: true,
-              itemBuilder: (_, int index) => messages[index],
-              itemCount: messages.length,
-            ),
-          ),
-          Divider(height: 5.0), //new
-          Container( //new
-            decoration: BoxDecoration(
-                color: Theme.of(context).cardColor),
-            child: buildTextComposer(), //modified
-          ),
-        ],
       ),
     );
   }
 
-  Widget buildTextComposer() {
-    return new IconTheme(
-      data: new IconThemeData(color: Theme.of(context).accentColor),
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 10.0),
-        child: Row(
-          children: <Widget>[ Flexible(
-            child: TextField(
-              controller: textController,
-              onSubmitted: handleSubmitted,
-              decoration: InputDecoration.collapsed(
-                  hintText: "Escribe un mensaje"),
-              cursorColor: Color.fromRGBO(7, 94, 84, 50),
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(milliseconds: 500));
+
+    return new MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Color.fromRGBO(7, 94, 84, 50),
+              leading: IconButton(
+                  icon: Icon(
+                Icons.account_circle,
+                color: Colors.blue,
+                size: 40,
+              )),
+              title: Column(
+                verticalDirection: VerticalDirection.down,
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment(-1, 0),
+                    child: Text(
+                      'Contacto',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment(-1, 0),
+                    child: Text(
+                      'últ. vez hoy a la 1:42 p.m.',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                  ),
+                ],
+              ),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.videocam,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.call,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.more_vert,
+                    color: Colors.white,
+                  ),
+                )
+              ],
             ),
-          ),
-            Container( //new
+            body: Container(
+              child: StreamBuilder(
+                  stream: bd.collection("WhatsAppContacto").snapshots(),
+                  builder: (context, snapshot) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Flexible(
+                          child: ListView.builder(
+                            padding: EdgeInsets.all(20.0),
+                            reverse: false,
+                            itemBuilder: (context, index) => buildItem(
+                                index, snapshot.data.documents[index]),
+                            itemCount: snapshot.data.documents.length,
+                          ),
+                        ),
+                        Divider(height: 5.0),
+                        Container(
+                            decoration: BoxDecoration(
+                                color: Theme.of(context).cardColor),
+                            child: buildTextComposer()),
+                      ],
+                    );
+                  }),
+            )));
+  }
+
+  Widget buildTextComposer() {
+    return new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10.0),
+        decoration: BoxDecoration(
+          //color: Color.fromRGBO(10, 10, 10, .1),
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.insert_emoticon)
+            ),
+            Flexible(
+              child: TextField(
+                controller: textController,
+                onSubmitted: (String text) {
+                  insertar(textController.text);
+                  textController.clear();
+                },
+                decoration:
+                    InputDecoration.collapsed(hintText: "Escribe un mensaje"),
+                cursorColor: Color.fromRGBO(7, 94, 84, 50),
+              ),
+            ),
+            IconButton(
+                icon: Icon(Icons.attach_file)
+            ),
+            IconButton(
+                icon: Icon(Icons.camera_alt)
+            ),
+            Container(
+              //new
               margin: EdgeInsets.symmetric(horizontal: 4.0), //new
-              child: IconButton( //new
-                  icon: Icon(Icons.send),
-                  color: Color.fromRGBO(7, 94, 84, 50),//new
-                  onPressed: () {
-                    obtener();
-                    insertar(textController.text);
-                    handleSubmitted(textController.text);
-                  },
+              child: IconButton(
+                //new
+                icon: Icon(Icons.send),
+                color: Color.fromRGBO(7, 94, 84, 50), //new
+                onPressed: () {
+                  insertar(textController.text);
+                  textController.clear();
+                },
               ), //new
             ),
           ],
         ),
-      ),
-    );
-  }
 
-
-  void handleSubmitted(String text) {
-    textController.clear();
-    ChatMessage message = ChatMessage(
-      text: text,
-    );
-    setState(() {
-      messages.insert(0, message);
-    });
-  }
-
-  String obtener() {
-    var mensaje;
-    var documento = Firestore.instance.collection("WhatsAppContacto").document('$id');
-    documento.get().then( (document) {
-      mensaje = document["Mensaje"];
-    });
-    return mensaje;
-//    bd.collection("WhasAppContacto").getDocuments()
-//        .then((QuerySnapshot snapshot) {
-//          snapshot.documents.forEach((f) => print("${f.data}"));
-//    });
-  }
-
-}
-
-
-
-
-
-
-class ChatMessage extends StatelessWidget {
-
-  ChatMessage({this.text});
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.only(right: 5.0),
-            child: Icon(Icons.account_circle,
-              color: Colors.blue,
-              size: 40,)
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(10, 140, 80, 100),
-              borderRadius: BorderRadius.circular(17.0),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.only(top: 5.0,right: 20, left: 18),
-                  child: Text( name,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17,
-                    ),
-                  ),
-                ),
-
-                Container(
-                  margin: const EdgeInsets.only( right: 20, left: 18, bottom: 5.0),
-                  child: Text(text,
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
-
-
 
 void insertar(String mensaje) async {
-  await bd.collection("WhatsAppContacto").document('$id').setData({'Mensaje': mensaje});
-  id++;
+  await bd.collection("WhatsAppContacto").add({'Mensaje': mensaje});
 }
-
-String obtener() {
-  var mensaje;
-  var documento = Firestore.instance.collection("WhatsAppContacto").document('$id');
-  documento.get().then( (document) {
-    mensaje = document["Mensaje"];
-  });
-  return mensaje;
-//    bd.collection("WhasAppContacto").getDocuments()
-//        .then((QuerySnapshot snapshot) {
-//          snapshot.documents.forEach((f) => print("${f.data}"));
-//    });
-}
-
-void actualizar(String mensaje) async{
-  await bd.collection("WhatsAppContacto").document('-LuO4kLpLue4t_vcxOEK').updateData({'mensaje': mensaje});
-}
-
-
